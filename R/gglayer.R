ae097353a9e6c3aa7fb61b86067e6c4fc700f74a
statisticiansix <statistician.six@gmail.com>
2019-11-28 15:00:33 +0000

    adding functionality for different data sets

--- R/gglayer.R
+++ R/gglayer.R
@@ -1,11 +1,24 @@
 gglayer <- function(.data,layer_use,...){
-  additionalArgs <- as.list(match.call())[-c(1:3)]
+  additionalArgs <<- as.list(match.call())[-c(1:3)]
 
-  out <- .data %>%
-    mutate('plot'=map(plot,function(plot,layer_use,additionalArgs){
-      plot+
-        do.call(layer_use,additionalArgs)
-    },layer_use=layer_use,additionalArgs=additionalArgs)
-    )
+  if("data"%in%names(additionalArgs)){
+    updatedArgs <<- additionalArgs[names(additionalArgs)!='data']
+    out <- .data %>%
+      mutate('plot'=map(plot,function(plot,data,layer_use,updatedArgs){
+        plot+
+          exec(layer_use,!!!updatedArgs,data=(!!additionalArgs$data)[[1]])
+      },layer_use=layer_use,updatedArgs=updatedArgs)
+      )
+  }else{
+    updated <<- additionalArgs
+    out <- .data %>%
+      mutate('plot'=map(plot,function(plot,layer_use,additionalArgs){
+        plot+
+          exec(layer_use,!!!additionalArgs)
+      },layer_use=layer_use,additionalArgs=additionalArgs)
+      )
+  }
+  #
+
   return(out)
 }
