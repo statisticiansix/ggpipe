85b44d6303f55415418c1310cbd5a7e47c0b6d30
statisticiansix <statistician.six@gmail.com>
2019-12-01 23:16:26 +0000

    cleaning up

--- R/gglayer.R
+++ R/gglayer.R
@@ -1,10 +1,9 @@
 gglayer <- function(.data,layer_use,...){
-  additionalArgs <<- as.list(match.call())[-c(1:3)]
+  additionalArgs <- as.list(match.call())[-c(1:3)]
 
   if("data"%in%names(additionalArgs)){
     updatedArgs <<- additionalArgs[names(additionalArgs)!='data']
     additionalArgs$data = sym(quo_name(additionalArgs$data))
-    print(additionalArgs$data)
     out <- .data %>%
       mutate('plot'=map(plot,function(plot,data,layer_use,updatedArgs){
         plot+
@@ -12,7 +11,7 @@ gglayer <- function(.data,layer_use,...){
       },layer_use=layer_use,updatedArgs=updatedArgs)
       )
   }else{
-    updated <<- additionalArgs
+    updated <- additionalArgs
     out <- .data %>%
       mutate('plot'=map(plot,function(plot,layer_use,additionalArgs){
         plot+
--- R/ggpipe.R
+++ R/ggpipe.R
@@ -1,15 +1,5 @@
-ggpipe <- function(.data,column_name=data,nested=FALSE){
-  column_name <- quo_name(enquo(column_name))
-  print(column_name)
-  if(!nested){
-    out <- .data %>%
-      nest(!!column_name:=colnames(.data)[!colnames(.data)%in%dplyr::group_vars(.data)])%>%
-      mutate('plot'=map(!!sym(column_name),function(x)ggplot(x))
-      )
-  }else{
-    out <- .data %>%
-      mutate('plot'=map(!!sym(column_name),function(x)ggplot(x)))
-  }
-
+ggpipe <- function(.data,data_column=data){
+  out <- .data %>%
+      mutate('plot'=map(!!sym(quo_name(enquo(data_column))),function(x)ggplot(x)))
   return(out)
 }
